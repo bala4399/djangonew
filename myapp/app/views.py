baba204ea0d0fbf6646f420a6ff57332a497903a
bala4399 <balajimani849@gmail.com>
2023-06-13 10:53:45 +0530

    two tempates

--- myapp/app/views.py
+++ myapp/app/views.py
@@ -5,4 +5,7 @@ from django.http import HttpResponse
 def Index(request):
     return render(request,"index.html",{})
 
+def Index2(request):
+    return render(request,"index2.html",{})
+
 # Create your views here.
--- myapp/myapp/urls.py
+++ myapp/myapp/urls.py
@@ -20,5 +20,6 @@ from app import views
 
 urlpatterns = [
     path("",views.Index,name="Index"),
+    path("test",views.Index2,name="Index2"),
     path('admin/', admin.site.urls),
 ]
